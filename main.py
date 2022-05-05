513a6d53df65f4c2994d185d3592e07b81ed0d82
Morgan Jeon <ahrjskr@gmail.com>
2022-05-05 21:58:31 +0900

    afds

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from machine import Pin, PWM
 import utime
 
-DC_HIGH = 10
-DC_LOW = 8
-SERVO = 6
+DC_HIGH = 
+DC_LOW = 
+SERVO = 
+TRIG = 
+ECHO = 
 
 servo_pwm = PWM(Pin(SERVO))
 dc_high = PWM(Pin(DC_HIGH))
 dc_high = PWM(Pin(DC_LOW))
+trigger = Pin(TRIG, Pin.OUT)
+echo = Pin(ECHO, Pin.IN)
 
 def dc_init():
     dc_high.freq(1000)
@@ -35,23 +39,46 @@ def servo_angle(angle: int):
     servo_pwm.duty_ns(1500000 + angle*10000)
     utime.sleep(0.5)
 
+def ultra():
+    trigger.low()
+    utime.sleep_us(2)
+    trigger.high()
+    utime.sleep_us(5)
+    trigger.low()
+    while echo.value() == 0:
+        signaloff = utime.ticks_us()
+    while echo.value() == 1:
+        signalon = utime.ticks_us()
+    timepassed = signalon - signaloff
+    distance = (timepassed * 0.0343) / 2
+    return distance # CM단위
+
+def left():
+    servo_angle(30)
+    utime.sleep(0.7)
+
+
+def right():
+    servo_angle(-30)
+    utime.sleep(0.7)
+
+def center():
+    servo_angle(0)
+    utime.sleep(0.7)
+
 def main():
     dc_init()
     servo_init()
-
-    dc_setSpeed(30000)
-    
-    servo_angle(30)
-    utime.sleep(1)
-    
     servo_angle(0)
-    utime.sleep(1)
-    
-    servo_angle(-30)
-	utime.sleep(1)
-    
-    servo_angle(0)
-    
-    dc_setSpeed(0)
+    dc_setSpeed(10000)
+    while True:
+        if ultra() < 10:
+            dc_setSpeed(0)
+            left()
+            dc_setSpeed(10000)
+            center()
+            right()
+            center()
+        utime.sleep(0.5)
 
 main()
